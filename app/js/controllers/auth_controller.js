ec800eb60eb0a72a4dba00f96481f3ddf44c02bf
AkyunaAkish <akyunaakish@gmail.com>
2016-05-14 21:42:42 -0600

    updated auth page styling and gitignore

--- app/js/controllers/auth_controller.js
+++ app/js/controllers/auth_controller.js
@@ -11,7 +11,19 @@
   ];
 
   function AuthController ($log, $scope, authService) {
-    
+    $scope.vm = {};
+    $scope.vm.signup = true;
+
+    $scope.vm.showLogin = function(){
+    $scope.vm.signup = false;
+    $scope.vm.login = true;
+    }
+
+    $scope.vm.showSignup = function(){
+      $scope.vm.signup = true;
+      $scope.vm.login = false;
+    }
+
     socket.emit('messageFeed', 'yo');
   }
 
